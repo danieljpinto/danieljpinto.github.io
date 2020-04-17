1544f7e85c09c215024846197e8917203fcfd1e9
Daniel Jose Pinto (MB Pro) <daniel.j.pinto@gmail.com>
2020-04-17 10:24:16 +0200

    update cv

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,16 @@ window.onresize = function() {
 		callback();
 	}, 100);
 };
+
+function getAge(dateString) 
+{
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) 
+    {
+        age--;
+    }
+    return age;
+}
